be79906173bcf43c24a47ea49cda2894e287ceca
heeo-h <iglosslove0@gmail.com>
2024-12-02 14:42:08 +0900

    스크립트 분리 작업
    R_Hand, L_Hand, Button_Control 스크립트
    스탠드/테이블 분리작업

---
+++ Assets/Project/Scripts/Table_Scene_Script/R_Hand_2.cs
@@ -0,0 +1,255 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class R_Hand_2 : MonoBehaviour
+{
+    GameObject Rhand;
+    LineRenderer lineR;
+    Ray ray;
+    RaycastHit hit;
+    public UnityEvent<bool> changeColor01 = new UnityEvent<bool>();
+    bool hitButton01;
+    public UnityEvent<bool> changeColor02 = new UnityEvent<bool>();
+    bool hitButton02;
+    //public UnityEvent<bool> changeColor03 = new UnityEvent<bool>();
+    //bool hitButton03;
+    //public UnityEvent<bool> changeColor04 = new UnityEvent<bool>();
+    //bool hitButton04;
+    //public UnityEvent<bool> changeColor05 = new UnityEvent<bool>();
+    //bool hitButton05;
+    //public UnityEvent<bool> changeColor06 = new UnityEvent<bool>();
+    //bool hitButton06;
+    Coroutine vibeHandle;
+
+    //GameObject startScreen;
+    //GameObject selectScreen;
+    //GameObject tutorialScreen;
+    //GameObject optionScreen;
+    GameObject cameraCenter;
+
+    //bool optionSwitch; //옵션창 열림,닫힘을 확인하기위해 
+
+    L_Hand_2 lHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rhand = GameObject.Find("RightHandAnchor");
+        transform.position = Rhand.transform.position;
+        transform.eulerAngles = Rhand.transform.eulerAngles;
+        transform.parent = Rhand.transform;
+
+        lineR = GetComponent<LineRenderer>();
+
+        //startScreen = GameObject.Find("StartScreen");
+        //selectScreen = GameObject.Find("SelectScreen");
+        //selectScreen.SetActive(false);
+        //tutorialScreen = GameObject.Find("TutorialScreen");
+        //optionScreen = GameObject.Find("OptionScreen");
+        cameraCenter = GameObject.Find("CenterEyeAnchor");
+        //optionScreen.SetActive(false); //시작할때 옵션창 비활성화
+        lHand = GameObject.Find("leftHand").GetComponent<L_Hand_2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray.origin = transform.position;
+        ray.direction = transform.forward;
+        lineR.SetPosition(0, ray.origin);
+        lineR.SetPosition(1, ray.direction * 8);
+        lineR.material.color = Color.cyan;
+
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            lineR.SetPosition(0, ray.origin);
+            lineR.SetPosition(1, hit.point);
+
+            //if (hit.collider.gameObject.CompareTag("startscreen"))
+            //{
+            //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            //    {
+            //        hit.collider.gameObject.SetActive(false);
+            //    }
+            //}
+
+            if (hit.collider.gameObject.CompareTag("button01")) //시작으로 돌아가는 버튼
+            {
+                if (hitButton01 == false)
+                {
+                    hitButton01 = true;
+                    changeColor01.Invoke(hitButton01);
+                }
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+                {
+                    hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+                    StartCoroutine(VibeHandle());
+                }
+            }
+            if (hit.collider.gameObject.CompareTag("button02")) //프로그램을 종료하는 버튼
+            {
+                if (hitButton02 == false)
+                {
+                    hitButton02 = true;
+                    changeColor02.Invoke(hitButton02);
+                }
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+                {
+                    hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+                    StartCoroutine(VibeHandle());
+                }
+            }
+            //if (hit.collider.gameObject.CompareTag("button03"))
+            //{
+            //    if (hitButton03 == false)
+            //    {
+            //        hitButton03 = true;
+            //        changeColor03.Invoke(hitButton03);
+            //    }
+
+            //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            //    {
+            //        hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+            //        StartCoroutine(VibeHandle());
+            //    }
+            //}
+
+            //if (hit.collider.gameObject.CompareTag("off"))
+            //{
+            //    if (hitButton06 == false)
+            //    {
+            //        hitButton06 = true;
+            //        changeColor06.Invoke(hitButton06);
+            //    }
+
+            //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            //    {
+            //        hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+            //        StartCoroutine(VibeHandle());
+            //    }
+            //}
+
+            //if (hit.collider.gameObject.CompareTag("stand"))
+            //{
+            //    if (hitButton04 == false)
+            //    {
+            //        hitButton04 = true;
+            //        changeColor04.Invoke(hitButton04);
+            //    }
+            //    if (hitButton05 == true)
+            //    {
+            //        hitButton05 = false;
+            //        changeColor05.Invoke(hitButton05);
+            //    }
+
+            //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            //    {
+            //        hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+            //        StartCoroutine(VibeHandle());
+            //    }
+            //}
+            //if (hit.collider.gameObject.CompareTag("table"))
+            //{
+            //    if (hitButton05 == false)
+            //    {
+            //        hitButton05 = true;
+            //        changeColor05.Invoke(hitButton05);
+            //    }
+            //    if (hitButton04 == true)
+            //    {
+            //        hitButton04 = false;
+            //        changeColor04.Invoke(hitButton04);
+            //    }
+
+            //    if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+            //    {
+            //        hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+            //        StartCoroutine(VibeHandle());
+            //    }
+            //}
+            if (hit.collider.gameObject.CompareTag("option"))
+            {
+                hitButton01 = false;
+                changeColor01.Invoke(hitButton01);
+                hitButton02 = false;
+                changeColor02.Invoke(hitButton02);
+            }
+        }
+        else if (hitButton01 == true)
+        {
+            hitButton01 = false;
+            changeColor01.Invoke(hitButton01);
+        }
+        else if (hitButton02 == true)
+        {
+            hitButton02 = false;
+            changeColor02.Invoke(hitButton02);
+        }
+        //else if (hitButton03 == true)
+        //{
+        //    hitButton03 = false;
+        //    changeColor03.Invoke(hitButton03);
+        //}
+        //else if (hitButton04 == true)
+        //{
+        //    hitButton04 = false;
+        //    changeColor04.Invoke(hitButton04);
+        //}
+        //else if (hitButton05 == true)
+        //{
+        //    hitButton05 = false;
+        //    changeColor05.Invoke(hitButton05);
+        //}
+        //else if (hitButton06 == true)
+        //{
+        //    hitButton06 = false;
+        //    changeColor06.Invoke(hitButton06);
+        //}
+
+        //if (startScreen.activeSelf == false)
+        //{
+        //    selectScreen.SetActive(true);
+        //}
+
+        //if (tutorialScreen.activeSelf == true)
+        //{
+        //    selectScreen.SetActive(false);
+        //}
+        //else
+        //{
+        //    selectScreen.SetActive(true);
+        //}
+
+        if (lHand.optionScreen.activeSelf == true && lHand.optionSwitch == true) //옵션창이 열려있을때
+        {
+            if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+            {
+                lHand.optionScreen.SetActive(false);
+                lHand.optionSwitch = false;
+            }
+        }
+        else if (lHand.optionScreen.activeSelf == false && lHand.optionSwitch == false) //옵션창이 닫혀있을때
+        {
+            if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+            {
+                lHand.optionScreen.SetActive(true);
+                lHand.optionSwitch = true;
+                lHand.optionScreen.transform.position = cameraCenter.transform.position + cameraCenter.transform.forward * 1;
+                lHand.optionScreen.transform.forward = cameraCenter.transform.forward;
+                //옵션창이 현재 사용자가 바라보는 방향의 앞쪽에 나올수있도록
+            }
+        }
+    }
+
+    IEnumerator VibeHandle()
+    {
+        OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.RTouch);
+        yield return new WaitForSeconds(0.5f);
+        OVRInput.SetControllerVibration(0f, 0f, OVRInput.Controller.RTouch);
+    }
+}
