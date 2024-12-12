eeaf261148004c1c40e51b4e0d5e83c8e386d387
LYS93 <yslee93930@gmail.com>
2024-12-12 17:14:35 +0900

    자자 버스안에서

--- Assets/Project/Scripts/Button_Control.cs
+++ Assets/Project/Scripts/Button_Control.cs
@@ -7,18 +7,37 @@ public class Button_Control : MonoBehaviour
 {
     public GameObject panelStart;
     public GameObject panelMain;
+    public GameObject panelCoffee;
+    public GameObject panelTea;
+    public GameObject panelFood;
+    public GameObject panelCoffeemenu;
+
+    bool startSwitch;
     void Start()
     {
         panelStart = GameObject.Find("Panel(start)");
         panelStart.SetActive(true);
         panelMain = GameObject.Find("Panel(MainScreen)");
         panelMain.SetActive(false);
+        panelCoffee = GameObject.Find("Panel(CoffeeLatteSelect)");
+        panelCoffee.SetActive(false);
+        panelTea = GameObject.Find("Panel(TeaAdeSelect)");
+        panelTea.SetActive(false);
+        panelFood = GameObject.Find("Panel(FoodSelect)");
+        panelFood.SetActive(false);
+        panelCoffeemenu = GameObject.Find("Panel(CoffeeMenu)");
+        panelCoffeemenu.SetActive(false);
     }
 
     
     void Update()
     {
-        
+        if (panelMain.activeSelf == true && startSwitch == false)
+        {
+            panelCoffee.SetActive(true);
+            panelCoffeemenu.SetActive(true);
+            startSwitch = true;
+        }
     }
     public void Restart() //시작화면으로 돌아가는 버튼
     {
@@ -37,5 +56,33 @@ public class Button_Control : MonoBehaviour
     {
         panelMain.SetActive(true);
         panelStart.SetActive(false);
+    }
+    public void Gostart()
+    {
+        panelMain.SetActive(false);
+        panelCoffee.SetActive(false);
+        panelTea.SetActive(false);
+        panelFood.SetActive(false);
+        panelCoffeemenu.SetActive(false);
+        panelStart.SetActive(true);
+        startSwitch = false;
+    }
+    public void CoffeeMenu()
+    {
+        panelCoffee.SetActive(true);
+        panelTea.SetActive(false);
+        panelFood.SetActive(false);
+    }
+    public void TeaMenu()
+    {
+        panelCoffee.SetActive(false);
+        panelTea.SetActive(true);
+        panelFood.SetActive(false);
+    }
+    public void FoodMenu()
+    {
+        panelCoffee.SetActive(false);
+        panelTea.SetActive(false);
+        panelFood.SetActive(true);
     }
 }
