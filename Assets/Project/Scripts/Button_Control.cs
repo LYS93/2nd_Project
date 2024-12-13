ab66e586c72d8fe97605391cb7c1dae98b059f15
LYS93 <yslee93930@gmail.com>
2024-12-13 12:56:16 +0900

    이거 이거 많다 많아......

--- Assets/Project/Scripts/Button_Control.cs
+++ Assets/Project/Scripts/Button_Control.cs
@@ -11,8 +11,16 @@ public class Button_Control : MonoBehaviour
     public GameObject panelTea;
     public GameObject panelFood;
     public GameObject panelCoffeemenu;
+    public GameObject panelLattemenu;
+    public GameObject panelTeamenu;
+    public GameObject panelAdemenu;
+    public GameObject panelNonCoffeemenu;
+    public GameObject panelFrappemenu;
+    public GameObject panelFoodmenu;
 
     bool startSwitch;
+    bool teaselectSwitch;
+    bool foodselectSwitch;
     void Start()
     {
         panelStart = GameObject.Find("Panel(start)");
@@ -27,16 +35,43 @@ public class Button_Control : MonoBehaviour
         panelFood.SetActive(false);
         panelCoffeemenu = GameObject.Find("Panel(CoffeeMenu)");
         panelCoffeemenu.SetActive(false);
+        panelLattemenu = GameObject.Find("Panel(LatteMenu)");
+        panelLattemenu.SetActive(false);
+        panelTeamenu = GameObject.Find("Panel(TeaMenu)");
+        panelTeamenu.SetActive(false);
+        panelAdemenu = GameObject.Find("Panel(AdeMenu)");
+        panelAdemenu.SetActive(false);
+        panelNonCoffeemenu = GameObject.Find("Panel(NonCoffeeLatteMenu)");
+        panelNonCoffeemenu.SetActive(false);
+        panelFrappemenu = GameObject.Find("Panel(FrappeMenu)");
+        panelFrappemenu.SetActive(false);
+        panelFoodmenu = GameObject.Find("Panel(FoodMenu)");
+        panelFoodmenu.SetActive(false);
     }
 
     
     void Update()
     {
-        if (panelMain.activeSelf == true && startSwitch == false)
+        if (panelMain.activeSelf == true && startSwitch == false && panelCoffee == true)
         {
-            panelCoffee.SetActive(true);
             panelCoffeemenu.SetActive(true);
+            panelTeamenu.SetActive(false);
+            panelFoodmenu.SetActive(false);
             startSwitch = true;
+        }
+        if(panelMain.activeSelf == true && teaselectSwitch == false && panelTea == true)
+        {
+            panelCoffeemenu.SetActive(false);
+            panelTeamenu.SetActive(true);
+            panelFoodmenu.SetActive(false);
+            teaselectSwitch = true;
+        }
+        if (panelMain.activeSelf == true && foodselectSwitch == false && panelFood == true)
+        {
+            panelCoffeemenu.SetActive(false);
+            panelTeamenu.SetActive(false);
+            panelFoodmenu.SetActive(true);
+            foodselectSwitch = true;
         }
     }
     public void Restart() //시작화면으로 돌아가는 버튼
@@ -56,6 +91,7 @@ public class Button_Control : MonoBehaviour
     {
         panelMain.SetActive(true);
         panelStart.SetActive(false);
+        panelCoffee.SetActive(true);
     }
     public void Gostart()
     {
@@ -64,6 +100,11 @@ public class Button_Control : MonoBehaviour
         panelTea.SetActive(false);
         panelFood.SetActive(false);
         panelCoffeemenu.SetActive(false);
+        panelLattemenu.SetActive(false);
+        panelTeamenu.SetActive(false);
+        panelAdemenu.SetActive(false);
+        panelNonCoffeemenu.SetActive(false);
+        panelFrappemenu.SetActive(false);
         panelStart.SetActive(true);
         startSwitch = false;
     }
@@ -72,17 +113,62 @@ public class Button_Control : MonoBehaviour
         panelCoffee.SetActive(true);
         panelTea.SetActive(false);
         panelFood.SetActive(false);
+        teaselectSwitch = false;
+        foodselectSwitch = false;
     }
     public void TeaMenu()
     {
         panelCoffee.SetActive(false);
         panelTea.SetActive(true);
         panelFood.SetActive(false);
+        startSwitch = false;
+        foodselectSwitch = false;
     }
     public void FoodMenu()
     {
         panelCoffee.SetActive(false);
         panelTea.SetActive(false);
         panelFood.SetActive(true);
+        panelFoodmenu.SetActive(true);
+        startSwitch = false;
+        teaselectSwitch = false;
+    }
+    public void CoffeeOpen()
+    {
+        panelCoffeemenu.SetActive(true);
+        panelLattemenu.SetActive(false);
+    }
+    public void LatteOpen()
+    {
+        panelCoffeemenu.SetActive(false);
+        panelLattemenu.SetActive(true);
+    }
+    public void TeaOpen()
+    {
+        panelTeamenu.SetActive(true);
+        panelAdemenu.SetActive(false);
+        panelNonCoffeemenu.SetActive(false);
+        panelFrappemenu.SetActive(false);
+    }
+    public void AdeOpen()
+    {
+        panelTeamenu.SetActive(false);
+        panelAdemenu.SetActive(true);
+        panelNonCoffeemenu.SetActive(false);
+        panelFrappemenu.SetActive(false);
+    }
+    public void NonCoffeeOpen()
+    {
+        panelTeamenu.SetActive(false);
+        panelAdemenu.SetActive(false);
+        panelNonCoffeemenu.SetActive(true);
+        panelFrappemenu.SetActive(false);
+    }
+    public void FrappeOpen()
+    {
+        panelTeamenu.SetActive(false);
+        panelAdemenu.SetActive(false);
+        panelNonCoffeemenu.SetActive(false);
+        panelFrappemenu.SetActive(true);
     }
 }
